d5eeb808eab4e6939a4ccf2351c0e5538a8211d3
Collins Ruto <collinsruto48@gmail.com>
2024-02-14 07:26:19 -0800

    Delete fish/conf.d/virtualfish-loader.fish

--- fish/conf.d/virtualfish-loader.fish
+++
@@ -1,4 +0,0 @@
-set -g VIRTUALFISH_VERSION 2.5.5
-set -g VIRTUALFISH_PYTHON_EXEC /home/collins/.local/pipx/venvs/virtualfish/bin/python
-source /home/collins/.local/pipx/venvs/virtualfish/lib/python3.11/site-packages/virtualfish/virtual.fish
-emit virtualfish_did_setup_plugins
