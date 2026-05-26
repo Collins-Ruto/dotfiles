0fc04d7ca897aa14737bed593ff443a923b3a11a
collins-ruto <collinsruto48@gmail.com>
2026-05-26 09:00:00 +0300

    feat(yazi): add plugins and configure keymaps

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,3 +1,22 @@
 require("full-border"):setup({
 	type = ui.Border.ROUNDED, -- Tells Yazi to look for corner structures
 })
+
+require("githead"):setup({
+	branch_prefix = "on",
+	branch_symbol = " ",
+	branch_borders = "()",
+})
+
+require("fg"):setup({
+	default_action = "menu", -- nvim, jump
+})
+
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
